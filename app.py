eeca17fe9f45fcf41d79c5a5ccfee8d9972dd780
Surabhi21072001 <149667922+Surabhi21072001@users.noreply.github.com>
2025-07-15 13:29:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -238,4 +238,7 @@ def download_ready_images():
     return jsonify({"results": results})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
